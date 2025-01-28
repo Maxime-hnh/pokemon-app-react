d41d62cfeb3ca31ad3cc522f01b7f818246165e6
Maxime <maxime.huynh@okolo.fr>
2025-01-28 21:26:31 +0100

    add env

--- src/_config/firebaseConfig.js
+++ src/_config/firebaseConfig.js
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBzjm7pASl5SAtInnVYu8OEU8UXJmrLT1o",
-  authDomain: "pokemon-app-53bb7.firebaseapp.com",
-  projectId: "pokemon-app-53bb7",
-  storageBucket: "pokemon-app-53bb7.firebasestorage.app",
-  messagingSenderId: "699970092149",
-  appId: "1:699970092149:web:3a56a03d578c1234074c3c",
-  measurementId: "G-113VQ7XQ21"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAINE,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MESUREMENTID
 };
 
 // Initialize Firebase
